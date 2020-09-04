054975e00672d9fe58e18f3044344c48190a228e
Bruno Mikoski <badawe@gmail.com>
2020-09-04 11:05:14 +0100

    fix: naming to remove warning

--- Scripts/Runtime/ResourceScriptableObjectSingleton.cs
+++ Scripts/Runtime/ResourceScriptableObjectSingleton.cs
@@ -17,9 +17,9 @@ namespace BrunoMikoski.ScriptableObjectCollections.Core
         }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        public static T LoadOrCreateInstance<T>() where T: ScriptableObject
+        public static T2 LoadOrCreateInstance<T2>() where T2 : ScriptableObject
         {
-            T newInstance = Resources.Load<T>(typeof(T).Name);
+            T2 newInstance = Resources.Load<T2>(typeof(T2).Name);
 
             if (newInstance != null)
                 return newInstance;
@@ -28,22 +28,22 @@ namespace BrunoMikoski.ScriptableObjectCollections.Core
             if (Application.isPlaying)
                 return null;
             
-            string registryGUID = UnityEditor.AssetDatabase.FindAssets($"t:{typeof(T).Name}")
+            string registryGUID = UnityEditor.AssetDatabase.FindAssets($"t:{typeof(T2).Name}")
                 .FirstOrDefault();
 
             if (!string.IsNullOrEmpty(registryGUID))
             {
-                newInstance = (T) UnityEditor.AssetDatabase.LoadAssetAtPath<ScriptableObject>(
+                newInstance = (T2) UnityEditor.AssetDatabase.LoadAssetAtPath<ScriptableObject>(
                     UnityEditor.AssetDatabase.GUIDToAssetPath(registryGUID));
             }
 
             if (newInstance != null)
-                return newInstance;
+                return newInstance ;
             
-            newInstance = CreateInstance<T>();
+            newInstance = CreateInstance<T2>();
 
             AssetDatabaseUtils.CreatePathIfDontExist("Assets/Resources");
-            UnityEditor.AssetDatabase.CreateAsset(newInstance, $"Assets/Resources/{typeof(T).Name}.asset");
+            UnityEditor.AssetDatabase.CreateAsset(newInstance, $"Assets/Resources/{typeof(T2).Name}.asset");
             UnityEditor.AssetDatabase.SaveAssets();
             UnityEditor.AssetDatabase.Refresh();
             return newInstance;

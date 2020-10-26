7724ec37d31b63321fefb121eeb22e4c86f1c00a
Bruno Mikoski <badawe@gmail.com>
2020-10-26 15:23:12 +0000

    fix: indirect reference issue

--- Scripts/Editor/CollectableIndirectReferenceDrawer.cs
+++ Scripts/Editor/CollectableIndirectReferenceDrawer.cs
@@ -10,21 +10,22 @@ namespace BrunoMikoski.ScriptableObjectCollections
         private const string COLLECTABLE_GUID_PROPERTY_PATH = "collectableGUID";
         private const string COLLECTION_GUID_PROPERTY_PATh = "collectionGUID";
 
-        
+
         private SerializedProperty objectAssetProperty;
         private SerializedProperty collectableGUIDProperty;
         private SerializedProperty collectionGUIDProperty;
 
         private CollectableScriptableObject collectableScriptableObject;
-        
+        private CollectableScriptableObject cachedReference;
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             objectAssetProperty = property.FindPropertyRelative(OBJECT_ASSET_PROPERTY_PATH);
             collectableGUIDProperty = property.FindPropertyRelative(COLLECTABLE_GUID_PROPERTY_PATH);
             collectionGUIDProperty = property.FindPropertyRelative(COLLECTION_GUID_PROPERTY_PATh);
 
-            if (objectAssetProperty.exposedReferenceValue == null
-                && !string.IsNullOrEmpty(collectableGUIDProperty.stringValue)
+            if (cachedReference == null 
+                && !string.IsNullOrEmpty(collectableGUIDProperty.stringValue) 
                 && !string.IsNullOrEmpty(collectionGUIDProperty.stringValue))
             {
                 if (CollectionsRegistry.Instance.TryGetCollectionByGUID(collectionGUIDProperty.stringValue,
@@ -33,29 +34,36 @@ namespace BrunoMikoski.ScriptableObjectCollections
                     if (collection.TryGetCollectableByGUID(collectableGUIDProperty.stringValue,
                         out CollectableScriptableObject collectable))
                     {
-                        objectAssetProperty.objectReferenceValue = collectable;
+                        cachedReference = collectable;
                     }
                 }
             }
             
-            using (EditorGUI.ChangeCheckScope scope = new EditorGUI.ChangeCheckScope())
+            
+            EditorGUI.PropertyField(position, objectAssetProperty, label, true);
+            if (objectAssetProperty.exposedReferenceValue != cachedReference)
             {
-                EditorGUI.PropertyField(position, objectAssetProperty, label, true);
-
-                if (scope.changed)
+                string collectableGUID = string.Empty;
+                string collectionGUID = string.Empty;
+            
+                if (objectAssetProperty.objectReferenceValue != null &&
+                    objectAssetProperty.objectReferenceValue is CollectableScriptableObject collectable)
                 {
-                    string collectableGUID = string.Empty;
-                    string collectionGUID = string.Empty;
-                        
-                    if (objectAssetProperty.objectReferenceValue != null && objectAssetProperty.objectReferenceValue is CollectableScriptableObject collectable)
-                    {
-                        collectableGUID = collectable.GUID;
-                        collectionGUID = collectable.Collection.GUID;
-                    }
-                    collectableGUIDProperty.stringValue = collectableGUID;
-                    collectionGUIDProperty.stringValue = collectionGUID;
+                    collectableGUID = collectable.GUID;
+                    collectionGUID = collectable.Collection.GUID;
+                    cachedReference = collectable;
                 }
+                else
+                {
+                    cachedReference = null;
+                }
+            
+                collectableGUIDProperty.stringValue = collectableGUID;
+                collectionGUIDProperty.stringValue = collectionGUID;
+                objectAssetProperty.serializedObject.ApplyModifiedProperties();
             }
         }
+
     }
 }
+

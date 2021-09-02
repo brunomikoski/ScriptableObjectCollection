3496f6d31aabb3dee133424c06517d59850cb05c
Duncan Waterreus <duncan@chimp.works>
2021-09-02 15:46:12 +0200

    Fix T is also used as the Type variable of the class by using TInstance instead of T

--- Scripts/Runtime/Core/ResourceScriptableObjectSingleton.cs
+++ Scripts/Runtime/Core/ResourceScriptableObjectSingleton.cs
@@ -17,17 +17,17 @@ namespace BrunoMikoski.ScriptableObjectCollections.Core
         }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        public static T LoadOrCreateInstance<T>() where T : ScriptableObject
+        public static TInstance LoadOrCreateInstance<TInstance>() where TInstance : ScriptableObject
         {
-            if (!TryToLoadInstance<T>(out T resultInstance))
+            if (!TryToLoadInstance<TInstance>(out TInstance resultInstance))
             {
 #if !UNITY_EDITOR
                 return null;
 #else
-                resultInstance = CreateInstance<T>();
+                resultInstance = CreateInstance<TInstance>();
 
                 AssetDatabaseUtils.CreatePathIfDontExist("Assets/Resources");
-                UnityEditor.AssetDatabase.CreateAsset(resultInstance, $"Assets/Resources/{typeof(T).Name}.asset");
+                UnityEditor.AssetDatabase.CreateAsset(resultInstance, $"Assets/Resources/{typeof(TInstance).Name}.asset");
                 UnityEditor.AssetDatabase.SaveAssets();
                 UnityEditor.AssetDatabase.Refresh();
                 return resultInstance;
@@ -42,9 +42,9 @@ namespace BrunoMikoski.ScriptableObjectCollections.Core
             return TryToLoadInstance<T>(out _);
         }
 
-        private static bool TryToLoadInstance<T>(out T result) where T: ScriptableObject
+        private static bool TryToLoadInstance<TInstance>(out TInstance result) where TInstance: ScriptableObject
         {
-            T newInstance = Resources.Load<T>(typeof(T).Name);
+            TInstance newInstance = Resources.Load<TInstance>(typeof(TInstance).Name);
 
             if (newInstance != null)
             {
@@ -53,12 +53,12 @@ namespace BrunoMikoski.ScriptableObjectCollections.Core
             }
 
 #if UNITY_EDITOR
-            string registryGUID = UnityEditor.AssetDatabase.FindAssets($"t:{typeof(T).Name}")
+            string registryGUID = UnityEditor.AssetDatabase.FindAssets($"t:{typeof(TInstance).Name}")
                 .FirstOrDefault();
 
             if (!string.IsNullOrEmpty(registryGUID))
             {
-                newInstance = (T) UnityEditor.AssetDatabase.LoadAssetAtPath<ScriptableObject>(
+                newInstance = (TInstance) UnityEditor.AssetDatabase.LoadAssetAtPath<ScriptableObject>(
                     UnityEditor.AssetDatabase.GUIDToAssetPath(registryGUID));
             }
 

05a5ba26a03981b005be3292a648131fd470badd
Bruno Mikoski <badawe@gmail.com>
2023-04-03 15:22:24 +0100

    add: base new item set

---
+++ Scripts/Editor/Core/ItemSetPropertyDrawer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace BrunoMikoski.ScriptableObjectCollections
+{
+  [CustomPropertyDrawer(typeof(ItemSet<>))]
+    public class ItemSetDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            
+        }
+    }
+}

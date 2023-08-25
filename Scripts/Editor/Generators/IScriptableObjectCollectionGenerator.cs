7d1c121accfc0e67d95b24a9e2de177b018b2051
Roy Theunissen <roy.theunissen@live.nl>
2023-08-25 15:02:34 +0200

    Added some extra descriptions for clarity.

--- Scripts/Editor/Generators/IScriptableObjectCollectionGenerator.cs
+++ Scripts/Editor/Generators/IScriptableObjectCollectionGenerator.cs
@@ -7,16 +7,28 @@ namespace BrunoMikoski.ScriptableObjectCollections
     {
     }
 
+    /// <summary>
+    /// Interface for classes that generate items for a Scriptable Object Collection.
+    /// </summary>
+    /// <typeparam name="CollectionType">The type of collection to generate items for.</typeparam>
+    /// <typeparam name="TemplateType">The template class that represents items to add/update.</typeparam>
     public interface IScriptableObjectCollectionGenerator<CollectionType, TemplateType>
         : IScriptableObjectCollectionGeneratorBase
         where CollectionType : ScriptableObjectCollection
         where TemplateType : ItemTemplate
     {
+        /// <summary>
+        /// If specified, any items that do not match the returned item templates get removed.
+        /// Return false if you want to generate items but allow people to manually add items of their own.
+        /// </summary>
         bool ShouldRemoveNonGeneratedItems { get; }
 
         void GetItemTemplates(List<TemplateType> templates, CollectionType collection);
     }
 
+    /// <summary>
+    /// Base class for templates that represent which items there should be in a collection.
+    /// </summary>
     public abstract class ItemTemplate
     {
         public string name;
